102a946e811ebef4a74926815a675d7c5bcf5359
haterade22 <36387120+haterade22@users.noreply.github.com>
2024-12-20 09:43:13 -0600

    Update CampaignStartGlobals.cs
    Signed-off-by: haterade22 <36387120+haterade22@users.noreply.github.com>

--- Main/CampaignStart/CampaignStartGlobals.cs
+++ Main/CampaignStart/CampaignStartGlobals.cs
@@ -28,6 +28,8 @@ namespace LOTRAOM.CampaignStart
             ["mirkwood"] = "town_EW1",
             ["lothlorien"] = "town_EW1",
             ["erebor"] = "town_EW1",
+            ["umbar"] = "town_ES1",
+            ["harad"] = "town_ES1",
         };
         public static void OnCharacterCreationFinalized()
         {
