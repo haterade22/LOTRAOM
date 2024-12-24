d77690034e26cb84036ac02c8958ac1909e6924f
haterade22 <36387120+haterade22@users.noreply.github.com>
2024-12-24 12:10:10 -0600

    Update CampaignStartGlobals.cs
    Changed everything for Cultures to EW1 Town (MT in Gondor). So you dont spawn in ocean or a place where the map isn't done. 
    
    Signed-off-by: haterade22 <36387120+haterade22@users.noreply.github.com>

--- Main/CampaignStart/CampaignStartGlobals.cs
+++ Main/CampaignStart/CampaignStartGlobals.cs
@@ -16,22 +16,22 @@ namespace LOTRAOM.CampaignStart
         public static Dictionary<string, string> StartingSettlement = new()
         {
             // if the mod keeps has multiple factions with same culture (like empire in vanilla) this code will have to be refactored
-            ["aserai"] = "town_A1",
-            ["empire"] = "town_EN1",
-            ["khuzait"] = "town_K1",
-            ["sturgia"] = "town_S1",
-            ["battania"] = "town_B1",
-            ["vlandia"] = "town_V1",
+            ["aserai"] = "town_EW1",
+            ["empire"] = "town_EW1",
+            ["khuzait"] = "town_EW1",
+            ["sturgia"] = "town_EW1",
+            ["battania"] = "town_EW1",
+            ["vlandia"] = "town_EW1",
             ["gondor"] = "town_EW1",
             ["mordor"] = "town_ES1",
             ["rivendell"] = "town_EW1",
             ["mirkwood"] = "town_EW1",
             ["lothlorien"] = "town_EW1",
             ["erebor"] = "town_EW1",
-            ["umbar"] = "town_ES1",
-            ["dolguldur"] = "town_ES1",
-            ["isengard"] = "town_ES1",
-            ["gundabad"] = "town_ES1",
+            ["umbar"] = "town_EW1",
+            ["dolguldur"] = "town_EW1",
+            ["isengard"] = "town_EW1",
+            ["gundabad"] = "town_EW1",
         };
         public static void OnCharacterCreationFinalized()
         {
