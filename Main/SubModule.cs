b1ad61fd2915880830a09ab53c7e8509a2b12037
haterade22 <36387120+haterade22@users.noreply.github.com>
2024-12-24 12:17:09 -0600

    Update SubModule.cs
    Changing the Start Age of Men to Enter The World of Arda
    
    Signed-off-by: haterade22 <36387120+haterade22@users.noreply.github.com>

--- Main/SubModule.cs
+++ Main/SubModule.cs
@@ -23,7 +23,7 @@ namespace LOTRAOM
 
             RemoveSandboxAndStoryOptions();
             Module.CurrentModule.AddInitialStateOption(
-                new InitialStateOption("LOTRAOM", name: new TextObject("Start Age of Man", null), 3,
+                new InitialStateOption("LOTRAOM", name: new TextObject("Enter The World of Arda", null), 3,
                 () => MBGameManager.StartNewGame(new LotrAOMCampaignManager()),
                 () => (Module.CurrentModule.IsOnlyCoreContentEnabled, new("Disabled during installation.", null)))
             );
