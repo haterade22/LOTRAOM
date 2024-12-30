b1615444dd0cea5d50bc9d03fa6fdbafddf6e3cb
haterade22 <36387120+haterade22@users.noreply.github.com>
2024-12-29 18:15:48 -0600

    Create CultureFeatsCampaignStartText.cs
    Signed-off-by: haterade22 <36387120+haterade22@users.noreply.github.com>

---
+++ Main/CultureFeats/CultureFeatsCampaignStartText.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
+using TaleWorlds.Core;
+
+namespace LOTRAOM.CultureFeats
+{
+    public class CultureFeatsCampaignStartText
+    {
+
+        public FeatObject exampleFeat;
+        public CultureFeatsCampaignStartText()
+        {
+            exampleFeat = Create("feat name");
+            InitializeAll();
+        }
+        private FeatObject Create(string stringId)
+        {
+            return Game.Current.ObjectManager.RegisterPresumedObject(new FeatObject(stringId));
+        }
+        private void InitializeAll()
+        {
+            exampleFeat.Initialize("{=!}loc", "{=localisation_string_id}repeated english localisation text for no reason, but idk, TaleWorlds does it this way", 0.20f, true, FeatObject.AdditionType.AddFactor);
+        }
+    }
+}
