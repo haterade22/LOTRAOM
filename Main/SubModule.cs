d74ae3ec432d72709bc3eff11e9c3b91db25292a
haterade22 <mikewilli22@gmail.com>
2025-02-03 13:03:24 -0600

    Updated to Enter The Age of Men

--- Main/SubModule.cs
+++ Main/SubModule.cs
@@ -24,7 +24,7 @@ namespace LOTRAOM
 
             RemoveSandboxAndStoryOptions();
             Module.CurrentModule.AddInitialStateOption(
-                new InitialStateOption("LOTRAOM", name: new TextObject("{=lotraom_start_game}Enter Age of Man", null), 3,
+                new InitialStateOption("LOTRAOM", name: new TextObject("{=lotraom_start_game}Enter The Age of Man", null), 3,
                 () => MBGameManager.StartNewGame(new LotrAOMCampaignManager()),
                 () => (Module.CurrentModule.IsOnlyCoreContentEnabled, new("Disabled during installation.", null)))
             );
