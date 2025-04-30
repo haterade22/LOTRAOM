4e1debe3fb054ee2ac02e52ef3dfe51a4a7a9da3
haterade22 <mikewilli22@gmail.com>
2025-04-30 15:57:52 -0500

    Edited out Race Stuff

--- Main/SubModule.cs
+++ Main/SubModule.cs
@@ -129,20 +129,8 @@ namespace LOTRAOM
             {
                 finishedFirstLoad = true;
                 RunManualPatches();
-                GetAllRaces();
             }
         }
-
-        private void GetAllRaces()
-        {
-            List<string> everyRace = new() { "human", "dwarf", "uruk_hai", "berserker", "uruk", "orc", "nazghul" };
-            foreach (string race in everyRace)
-            {
-                int raceId = TaleWorlds.Core.FaceGen.GetRaceOrDefault(race);
-                Globals.GetRaceStringIdFromInt.Add(raceId, race);
-            }
-        }
-
         private void RunManualPatches()
         {
 #pragma warning disable BHA0003 // Type was not found
