58c161dfd254361a9cf3b9d0c00d68ec2fb81ffe
actualAnian <dajams999@gmail.com>
2025-05-01 03:50:06 +0200

    switch MomentumGlobals to use a static Momentum multiplier

--- Main/Momentum/MomentumGlobals.cs
+++ Main/Momentum/MomentumGlobals.cs
@@ -23,12 +23,13 @@ namespace LOTRAOM.Momentum
             }
         }
         // momentum is multiplied by 100, to allow for more precision from battles, it is divided by 100 when shown in UI
-        public static int MaxMomentumFromBattleWon { get { return 30000; } } // battle casualties divided by total faction strength
-        public static int MomentumFromSiege { get { return 500; } }
-        public static int MomentumFromArmyGathering { get { return 200; } }
-        public static int MomentumFromVillageRaid { get { return 200; } }
-        public static int MomentumMultiplierFromTotalStrength { get { return 1000; } }
-        public static int MaxMomentumFromTotalStrength { get { return 4000; } } // caps when one faction is 4 times stronger than the other
+        public static int MomentumMultiplier { get { return 100; } }
+        public static int MaxMomentumFromBattleWon { get { return 300 * MomentumMultiplier; } } // battle casualties divided by total faction strength
+        public static int MomentumFromSiege { get { return 5 * MomentumMultiplier; } }
+        public static int MomentumFromArmyGathering { get { return 2 * MomentumMultiplier; } }
+        public static int MomentumFromVillageRaid { get { return 2 * MomentumMultiplier; } }
+        public static int MomentumMultiplierFromTotalStrength { get { return 10 * MomentumMultiplier; } }
+        public static int MaxMomentumFromTotalStrength { get { return 40 * MomentumMultiplier; } } // caps when one faction is 4 times stronger than the other
 
         public static float ScaleMomentum(float value)
         {
