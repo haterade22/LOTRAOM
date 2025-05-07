d0abfd9b1b46994596325d7768dda460f790c17b
haterade22 <mikewilli22@gmail.com>
2025-05-06 20:51:39 -0500

    Updated Isengard Event text

--- Main/Events/IsengardTextEvent.cs
+++ Main/Events/IsengardTextEvent.cs
@@ -18,15 +18,15 @@ namespace LOTRAOM.Events
         }
         public static List<IsengardTextEvent> All = new()
         {
-            new IsengardTextEvent("{CURRENT_DAY}", "Whispers from the Shire speak of old Gandalf the Grey visiting Hobbiton, his face grim with purpose. Strange business for a wizard in such quiet lands.", "Continue", 0.9f),
-            //new IsengardTextEvent("{CURRENT_DAY}", "Rangers in the North speak of shadowed figures skulking near the Shire. The Dúnedain watch the roads with wary eyes.", "Continue", 0.8f),
-            //new IsengardTextEvent("{CURRENT_DAY}", "Rumors drift from Isengard that the White Wizard has barred his gates. None have seen Gandalf since he rode south.", "Continue", 0.7f),
-            //new IsengardTextEvent("{CURRENT_DAY}", "Tales from Bree tell of black-cloaked riders on dark steeds, asking after hobbits. Fear grips the hearts of men in the taverns", "Continue", 0.6f),
-            //new IsengardTextEvent("{CURRENT_DAY}", "Horsemen of Rohan whisper of orc bands prowling the Westfold. The king’s hall is heavy with foreboding.", "Continue", 0.5f),
-           //new IsengardTextEvent("{CURRENT_DAY}", "Deep in Fangorn’s ancient heart, the felling of trees echoes like a wound. The forest stirs, its wrath kindling as if roused by some unseen hand.", "Continue", 0.4f),
-            new IsengardTextEvent("{CURRENT_DAY}", "The Fords of Isen lie parched, their waters vanished under Isengard’s gaze. Wayfarers murmur of foul craft at work in the shadow of the White Wizard’s tower.", "Continue", 0.3f),
-            //new IsengardTextEvent("{CURRENT_DAY}", "The Wild men of Dunland are seen gathering near Isengard’s walls. Rohan’s riders sharpen their spears", "Continue", 0.2f),
-            new IsengardTextEvent("{CURRENT_DAY}", "In Gondor, the beacons stand poised to blaze, their keepers vigilant. Across the Anduin, a nameless darkness gathers, and the hearts of Men steel for war.", "Continue", 0.1f)
+            new IsengardTextEvent("{CURRENT_DAY}", "Gossip of Gandalf the Grey visiting Bag End spreads across the Shire. His presence is welcomed, but curiosity of the friendly wizard’s reasons for making his way to Hobbiton lingers.", "Unexpected Company in Bag End", 0.9f),
+            //new IsengardTextEvent("{CURRENT_DAY}", "The Rangers of the North, led by Strider, confirm sightings of hooded, shadowy figures stalking the outskirts of the Shire. With a growing sense of worry and unease, the Rangers continue to keep a close eye on the unfamiliar visitors.", "Strider’s Reports", 0.8f),
+            //new IsengardTextEvent("{CURRENT_DAY}", "Accounts from travelers detail Saruman the White sealing the gates of Isengard not long after Gandalf the Grey was seen heading toward the fortress. The two events having correlation is unknown, but the travelers emphasize the oddity.", "Rumors from Isengard", 0.7f),
+            //new IsengardTextEvent("{CURRENT_DAY}", "An assistant at the Prancing Pony in Bree recalls the terrifying moment two figures dressed in all black raided the rooms assigned to a group of hobbits staying at the inn. Luckily, the party was not present during the attack. Unsure of its importance, the assistant notes that the same figures had come to the inn just days prior asking for a “Baggins”. He turned them away due to an uneasiness he felt about them.", "Raid at the Prancing Pony", 0.6f),
+            //new IsengardTextEvent("{CURRENT_DAY}", "The horsemen of Rohan exclaim their concern over the bands of orc prowling the Westfold leaving their king, Théoden, and his court distressed.", "A Warning from Rohan", 0.5f),
+           //new IsengardTextEvent("{CURRENT_DAY}", "A low, groaning wail from deep in Fangorn Forest accompanies the sounds of trees falling. While listening more intently, the noises seem to be those of ancient agony over the deforestation that is occurring.", "The Wailing in the Wind", 0.4f),
+            new IsengardTextEvent("{CURRENT_DAY}", "Those using the Fords of Isen for travel report on its sudden decrease in water levels and flow. Upon further investigation, the disturbance seems to come from the north near Isengard.", "Disturbance to the River Isen", 0.3f),
+            //new IsengardTextEvent("{CURRENT_DAY}", "Riders from Rohan are alarmed to find the Dunlendings accumulating outside of Isengard. With such lines being drawn and war growing closer, Théodred orders the preparation of weapons and forces.", "The Threat Grows", 0.2f),
+            new IsengardTextEvent("{CURRENT_DAY}", "With beacons made ready to be set ablaze, the watchful eyes in Gondor notice a large gathering just over the Great River. The stark realization that the time of war has come begins to set in.With beacons made ready to be set ablaze, the watchful eyes in Gondor notice a large gathering just over the Great River. The stark realization that the time of war has come begins to set in.", "A Darkness Beyond the Anduin", 0.1f)
         };
     }
 }
