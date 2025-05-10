6de01a65aa4226d804ed45fff8cb0b4575fad8d9
haterade22 <mikewilli22@gmail.com>
2025-05-10 09:56:01 -0500

    Updated some stuff

--- Main/CampaignStart/CampaignStartGlobals.cs
+++ Main/CampaignStart/CampaignStartGlobals.cs
@@ -66,16 +66,10 @@ namespace LOTRAOM.CampaignStart
             ["default"] = new CCCultureData(new List<string>() { "human", "uruk", "orc", "dwarf","uruk_hai" }, HumanBodyPropString) // in case culture is missing
         };
         private const string HumanBodyPropString = "<BodyProperties version=\"4\" age=\"22.35\" weight=\"0.5417\" build=\"0.5231\"  key=\"000DF00FC00033CD8771188F38770F8801F188778888888888888888546AF0F90088860308888888000000000000000000000000000000000000000043044144\"  />";
-
         private const string DunlandBodyPropString = "<BodyProperties version=\"4\" age=\"22.01\" weight=\"0.5139\" build=\"0.6435\"  key=\"0008F006CAFC1A54878F1FFF4F9F1EEE7F018877888888888888888F7718F5F5008A8603088888880000000000000000000000000000000000000000431C4142\"  />";
-
         private const string RohanBodyPropString = "<BodyProperties version=\"4\" age=\"22\" weight=\"0.5139\" build=\"0.6435\" key=\"0005280140001242947E068A709500460C7250703EB70F135C85021887733A070089B6030822BA9000000000000000000000000000000000000000003F1C7002\"  />";
-
-        private const string DwarfBodyPropString = "<BodyProperties version=\"4\" age=\"22.01\" weight=\"0.5648\" build=\"0.5347\"  key=\"0000040F0000300008801FFEFF8F0FFE109588878FF88067888878888D3010F30088860308FFF0AE000000000000000000000000000000000000000043040142\"  />";
-
+        private const string DwarfBodyPropString = "<BodyProperties version=\"4\" age=\"22.01\" weight=\"0.5648\" build=\"0.5347\"  key=\"000204000000314008801FFEFF8F0FF0109588878FF88067888878888D3010F30088860308FFF0AE000000000000000000000000000000000000000043040142\"  />";
         private const string GondorBodyPropString = "<BodyProperties version=\"4\" age=\"30.16\" weight=\"0\" build=\"0.7222\"  key=\"0015000D800013420F800A1A098007985FFD8660BE886A0167895C1885BB9A60009B06030909B70500000000000000000000000000000000000000003F3C2002\"  />";
-
         private const string MirkwoodBodyPropString = "<BodyProperties version=\"4\" age=\"30\" weight=\"0.037\" build=\"0.3634\"  key=\"00001C00000000031980F72F6853461660648330588B86016756B50C88A64A7500B516030B01EA0000000000000000000000000000000000000000003B6C4002\"  />";
-
     }
 }
